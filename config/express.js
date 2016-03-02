08133da4c370101164e1eec911ec31a866b761fd
Nasir Malik <nasir.malik@uac.edu.au>
2016-03-02 14:48:03 +1100

    Adding mongo test

--- config/express.js
+++ config/express.js
@@ -1,8 +1,23 @@
-var express = require('express');
+var config = require('./config'),
+    express = require('express'),
+    bodyParser = require('body-parser');
+
 module.exports = function() {
     var app = express();
-	app.set('views', './app/views');
-	app.set('view engine', 'ejs');
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    app.use(bodyParser.json());
+
+    app.set('views', './app/views');
+    app.set('view engine', 'ejs');
+
     require('../app/routes/index.server.routes.js')(app);
+    require('../app/routes/user.server.routes.js')(app);
+
+    app.use(express.static('./public'));
+
     return app;
 };
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-var port = 1337;
-var express = require('./config/express');
-var app = express();
-app.listen(port);
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var config = require('./config/config.js'),
+    mongoose = require('./config/mongoose.js'),
+    express = require('./config/express.js');
+
+var db = mongoose(),
+    app = express();
+
+app.listen(config.port);
+
 module.exports = app;
-console.log('Server running at http://localhost:' + port);
+console.log(process.env.NODE_ENV  + ' server running at http://localhost:' + config.port);
